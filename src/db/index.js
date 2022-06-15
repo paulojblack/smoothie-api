6a051edd33531d1acdad6d33793b5be595146de4
Paulo Black <pblack@goodwaygroup.com>
2022-06-15 10:06:16 -0400

    try/catch again

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,17 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_DATABASE_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
-    dialect: 'postgres'
-});
+let sequelize;
+
+try {
+
+    sequelize = new Sequelize(process.env.DB_DATABASE_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 5432,
+        dialect: 'postgres'
+    });
+} catch (e) {
+    throw new Error(e)
+}
 
 
 module.exports = sequelize
